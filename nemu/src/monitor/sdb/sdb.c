f48af836b1e9d37504140c967a838c3119e6981f
tracer-ics2022 <tracer@njuics.org>
2023-06-18 21:36:52 +0800

    >  compile NEMU 120211080425 XinYu Zhang Linux xan-VirtualBox 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:36:52 up  1:19,  9 users,  load average: 0.05, 0.04, 0.04

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,6 +55,14 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args) {
+	char *arg = strtok(NULL, " ");
+	int num;
+	sscanf(arg, "%d", &num);
+	cpu_exec(num);
+	return 0;	
+}
+
 static struct {
   const char *name;
   const char *description;
@@ -65,6 +73,7 @@ static struct {
   { "q", "Exit NEMU", cmd_q },
 
   /* TODO: Add more commands */
+	{ "si", "Single step", cmd_si },
 
 };
 
