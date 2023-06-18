b5a5fc956a80424183277cad8e72a7b2565b272c
tracer-ics2022 <tracer@njuics.org>
2023-06-18 22:36:42 +0800

    >  compile NEMU 120211080425 XinYu Zhang Linux xan-VirtualBox 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:42 up  2:18,  9 users,  load average: 0.01, 0.07, 0.04

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -31,7 +31,7 @@ void isa_reg_display() {
 	rbx            0xffffffffffffff01  -255
 	 */ 
 	for (int i = 0; i < 32; ++i) {
-		printf("%s: " FMT_WORD "%lu\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
+		printf("%s: " FMT_WORD "\t%lu\n", regs[i], cpu.gpr[i], cpu.gpr[i]);
 	}	
 }
 
